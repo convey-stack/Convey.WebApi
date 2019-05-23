5c5eb95acb356e4a14427a5ec0b63389916c600e
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-05-23 20:47:24 +0200

    Updated packages

--- src/Convey.WebApi/EndpointsBuilder.cs
+++ src/Convey.WebApi/EndpointsBuilder.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace Convey.WebApi
